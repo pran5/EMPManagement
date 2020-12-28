c37231eb19ebe2ba144a2d718376b706ba03e596
nikhilbhati <nikhilbhati44@gmail.com>
2020-12-28 22:12:24 +0530

    test

---
+++ EMPMUI/src/app/net-service.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Emp } from './EmpModle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NetServiceService {
+
+  constructor(private con: HttpClient) { }
+
+  allEmp(obj: any): Observable<any>
+  {
+    const url = 'http://localhost:8081/poc1';
+    return this.con.post(url, obj);
+  }
+  allEmpSpring(): Observable<any>
+  {
+    const url = 'http://localhost:7500/?x=133';
+    return this.con.get(url);
+  }
+}
