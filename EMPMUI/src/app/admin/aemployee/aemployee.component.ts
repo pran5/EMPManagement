2d48ac97b298d4bfab99a837f620f85b97561201
unknown <nikhilbhati44@gmail.com>
2021-01-24 11:28:28 +0530

    Admin connection with backend

--- EMPMUI/src/app/admin/aemployee/aemployee.component.ts
+++ EMPMUI/src/app/admin/aemployee/aemployee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Emp } from 'src/app/EmpModle';
+import { AdminserviceService } from 'src/app/adminservice.service';
 
 @Component({
   selector: 'app-aemployee',
@@ -7,12 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AemployeeComponent implements OnInit {
 
+  employee : Emp;
   selectedPaySlab = "";
   Gender = "";
   selectAccesLevel= "";
-  constructor() { }
+  constructor(private ser : AdminserviceService) {
+    this.employee = new Emp();
+   }
 
   ngOnInit(): void {
+  }
+
+  addEmp(em:Emp){
+
+    console.log("add clicked");
+    this.ser.insertEmp(em).subscribe(( data : any)=>{
+      console.log(data);
+    });
+  }
+
+  select(id:String){
+    console.log("select called");
+    this.ser.singleSelect(id).subscribe(( data : any)=>{
+      console.log(data);
+      this.employee = data;
+    });
+  }
+
+  update(em:Emp){
+    console.log("update clicked");
+    this.ser.updateEmp(em).subscribe(( data : any)=>{
+      console.log(data);
+    });
   }
 
 }
